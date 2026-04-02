114561604042260959768e8ed698a8a54218dc7c
hamedy-yahya-camara <94373474+hamedy-yahya-camara@users.noreply.github.com>
2026-04-02 17:07:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import requests
 from flask import Flask, jsonify, render_template
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,18 +10,15 @@ def hello_world():
 
 @app.route("/contact")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
-
+    return render_template("contact.html")
 
 @app.get("/paris")
 def api_paris():
     url = "https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&hourly=temperature_2m"
     response = requests.get(url)
     data = response.json()
-
     times = data.get("hourly", {}).get("time", [])
     temps = data.get("hourly", {}).get("temperature_2m", [])
-
     n = min(len(times), len(temps))
     result = [
         {"datetime": times[i], "temperature_c": temps[i]}
@@ -30,16 +26,15 @@ def api_paris():
     ]
     return jsonify(result)
 
-@app.route("/histogramme")
-def monhistogramme():
-    return render_template("histogramme.html")
-
-
 @app.route("/rapport")
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route("/histogramme")
+def monhistogramme():
+    return render_template("histogramme.html")
+
 # Ne rien mettre après ce commentaire
-    
+
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
